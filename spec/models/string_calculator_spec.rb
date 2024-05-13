9cd531145aff638281beced0d3945253b7a4c176
Adnan Ahmad <ahmadadnan1602@gmail.com>
2024-05-13 18:24:43 +0530

    write rspec for empty string and move to one and two numbers

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe StringCalculator, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".add" do
+    it "returns 0 for an empty string" do
+      result = StringCalculator.add("")
+      expect(result).to eq(0)
+    end
+
+    it "returns the number for a single number string" do
+      result = StringCalculator.add("1")
+      expect(result).to eq(1)
+    end
+
+    it "returns the sum of two numbers" do
+      result = StringCalculator.add("1,5")
+      expect(result).to eq(6)
+    end
+  end
 end
