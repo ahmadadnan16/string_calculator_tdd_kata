1827d4ccbe0c725172c70d62198dcdadb8d89111
Adnan Ahmad <ahmadadnan1602@gmail.com>
2024-05-13 19:01:07 +0530

    Handled new lines between numbers

--- spec/models/string_calculator_spec.rb
+++ spec/models/string_calculator_spec.rb
@@ -16,5 +16,9 @@ RSpec.describe StringCalculator, type: :model do
       result = StringCalculator.add("1,5")
       expect(result).to eq(6)
     end
+
+    def test_new_lines_between_numbers_returns_sum
+      assert_equal 6, StringCalculator.add("1\n2,3")
+    end
   end
 end
